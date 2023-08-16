62f5dcb59a6a7ca08ec90849b413a29ad141cfc8
Yelaman Yelmuratov <e99xxx@gmail.com>
2023-08-16 16:10:06 +0600

    upd: added dispose

--- lib/src/smart_face_camera.dart
+++ lib/src/smart_face_camera.dart
@@ -180,6 +180,7 @@ class _SmartFaceCameraState extends State<SmartFaceCamera> with WidgetsBindingOb
     }
 
     if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
       cameraController.stopImageStream();
     } else if (state == AppLifecycleState.resumed) {
       _startImageStream();
